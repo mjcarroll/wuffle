9123c1aa3b10e1c6bea8890f2f65585ed7feae10
Michael Carroll <michael@openrobotics.org>
2019-10-10 15:25:24 -0500

    OSRF Config

---
+++ packages/app/wuffle.config.js
@@ -0,0 +1,36 @@
+/**
+ * This defines board specific configuration.
+ *
+ * ### Name
+ *
+ * The name of your board, displayed in the board header.
+ *
+ *
+ * ### Columns
+ *
+ * Valid fields for columns are
+ *
+ *  * name       - unique name
+ *  * label      - label, if present on a GitHub issue column will be set
+ *  * closed     - if the column issues must be closed
+ *  * sorting    - true if column cards should be sorted
+ *                 incrementally based on links
+ *
+ * The default column is the column that holds open issues without
+ * any label constraints (Inbox in the example below).
+ *
+ */
+module.exports = {
+
+  name: 'ROS Board',
+
+  columns: [
+    { name: 'Inbox', label: null },
+    { name: 'Backlog', label: 'backlog', sorting: true },
+    { name: 'Ready', label: 'ready', sorting: true },
+    { name: 'In Progress', label: 'in progress', sorting: true },
+    { name: 'Needs Review', label: 'in review', sorting: true },
+    { name: 'Done', label: null, closed: true }
+  ]
+
+};
